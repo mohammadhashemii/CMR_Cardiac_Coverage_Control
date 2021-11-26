6f47e310236f0c785aa2eab47230fa2ce10fc48c
Mohammad Hashemi <mohammad99hashemi@gmail.com>
2021-11-26 20:45:51 +0330

    feat: lime now works

--- lime.py
+++ lime.py
@@ -1,4 +1,3 @@
-import os
 import argparse
 import copy
 import h5py
@@ -7,8 +6,14 @@ from augmentation_3D import Augmentation_3D
 import skimage.segmentation
 import numpy as np
 from utils import plot_volume
-from sklearn.model_selection import train_test_split
 import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+gpus = tf.config.experimental.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(gpus[0], True)
+device_name = tf.test.gpu_device_name()
+print("Available Devices: {}".format(device_lib.list_local_devices()))
+print("GPU name: {}".format(tf.test.gpu_device_name()))
 
 parser = argparse.ArgumentParser()
 
@@ -16,10 +21,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data_root', type=str, default='data/', help='path to the root of data directory')
 parser.add_argument('--dataset', type=str, default='dataset_APEX.hdf5', help='dataset name')
 parser.add_argument('--model_path', type=str, default='models/apex_model.h5', help='path to the model')
-parser.add_argument('--weights_path', type=str, default='weights/apex_weights.h5', help='path to the model weights for testing')
-parser.add_argument('--best_perturbations_dir', type=str, default='perturbations/', help='path to the perturbations directory')
+parser.add_argument('--weights_path', type=str, default='weights/_fold0_apex_weights.h5', help='path to the model weights for testing')
 
-parser.add_argument('--iterations', type=int, default=500, help='number of times which we want to apply different random perturbations')
+parser.add_argument('--iterations', type=int, default=1000, help='number of times which we want to apply different random perturbations')
 parser.add_argument('--transformations', type=list, default=['rotate', 'flip_horizontally', 'flip_vertically', 'brightness'], help='just some transformation supported')
 parser.add_argument('--n_pert', type=int, default=10, help='number of random generated perturbations for each sample')
 args = parser.parse_args()
@@ -27,9 +31,6 @@ args = parser.parse_args()
 # create data loader
 data_loader = DataLoader(hdf5_path=args.data_root + args.dataset)
 dset_x, dset_y = data_loader.read_data()
-
-if not os.path.exists(args.best_perturbations_dir):
-    os.makedirs(args.best_perturbations_dir)
 
 class Lime():
     def __init__(self, volume):
@@ -54,7 +55,6 @@ class Lime():
         return superpixels
 
     def generate_perturbations(self, superpixels) -> list:
-        print("Generating perturbations ...")
         assert superpixels.shape == self.volume.shape
 
         layers_perturbation = []
@@ -66,7 +66,6 @@ class Lime():
         return layers_perturbation
 
     def apply_perturbations(self, layers_perturbation: list, superpixels):
-        print("Applying perturbations on dataset ...")
 
         perturbed_volume = []
 
@@ -102,51 +101,71 @@ dataset = (
     .batch(1)       # since we want to have prediction for each sample
     .prefetch(2)
 )
+data_size = len(data)
 # we don't need these anymore
 del dset_x, dset_y, data
 
-for sample in dataset:
-    for i in range(args.n_pert):
-        volume = sample[0][0, :, :, :, 0]   # volume shape must be (128, 128, 3)
-        lime = Lime(volume)
-        superpixels = lime.generate_segmentation(max_iter=1000)
-        layers_perturbation = lime.generate_perturbations(superpixels)
-        perturbed_volume = lime.apply_perturbations(layers_perturbation, superpixels)
-        plot_volume(perturbed_volume)
-
-    if True:
-        break
-
-
-'''
-best_accuracy = 0.0
-best_perturbations = None
+correct_predicted_samples_X = []
+correct_predicted_samples_Y = []
+best_perturbations_X = []
+best_perturbations_Y = []
 idx = 0
-for i in range(args.iterations):
-    print('Iter %d/%d:' % (i, args.iterations))
-    print("================================")
-    segmentations = lime.generate_segmentation(max_iter=200)
-    volume_perturbations = lime.generate_perturbations(segmentations)
-    perturbed_volumes = lime.apply_perturbations(volume_perturbations, segmentations)
+with tf.device(device_name=device_name):
+    for sample in dataset:
+        print("============================")
+        print('Processing on sample:{}/{}'.format(idx, data_size))
+        volume = sample[0][0, :, :, :, 0]  # volume shape must be (128, 128, 3)
+        target = sample[1].numpy()[0, 0]
+        temp_volume = tf.expand_dims(volume, axis=3)
+        temp_volume = tf.expand_dims(temp_volume, axis=0)
+        pred = model.predict(temp_volume)[0,0]
+        if (pred >= 0.5 and target == 0) or (pred < 0.5 and target == 1): # wrong prediction and pass it
+            print("IGNORED. The prediction was wrong.")
+            idx += 1
+            continue
+        else:
+            correct_predicted_samples_X.append(volume)
+            correct_predicted_samples_Y.append(target)
+        lime = Lime(volume)
+        best_pred = 0.5
+        best_volume = None  # generated volume by lime with the most accurate prediction
+        best_idx = 0
+        for i in range(args.n_pert):
+            superpixels = lime.generate_segmentation(max_iter=args.iterations)
+            layers_perturbation = lime.generate_perturbations(superpixels)
+            perturbed_volume = lime.apply_perturbations(layers_perturbation, superpixels)
+            #plot_volume(perturbed_volume)
+            temp_volume = tf.expand_dims(perturbed_volume, axis=3)
+            temp_volume = tf.expand_dims(temp_volume, axis=0)
+            pred = model.predict(temp_volume)[0,0]
+            if target == 1:
+                if  pred > best_pred:
+                    best_pred = pred
+                    best_volume = volume
+                    best_idx = i
+            else:   # target = 0
+                if  pred < best_pred:
+                    best_pred = pred
+                    best_volume = volume
+                    best_idx = i
+        print("The {}/{} perturbation is chosen with prediction score: {}".format(best_idx, args.n_pert, best_pred))
+        best_perturbations_X.append(best_volume)
+        best_perturbations_Y.append(target)
+        idx += 1
 
-    print("Evaluating the model ...")
-    loss, accuracy = model.evaluate(dset_x)
-    if best_accuracy <= accuracy:
-        best_accuracy = accuracy
-        best_perturbations = volume_perturbations
-        idx = i
 
-print("Best accuracy: {}".format(best_accuracy))
-perturbations_path = args.best_perturbations_dir + 'perturbations_' + str(idx) + '.h5'
-print("Best perturbations saved at: {}".format(perturbations_path))
+correct_predicted_samples_X = np.array(correct_predicted_samples_X)
+correct_predicted_samples_Y = np.array(correct_predicted_samples_Y)
+best_perturbations_X = np.array(best_perturbations_X)
+best_perturbations_Y = np.array(best_perturbations_Y)
 
-with h5py.File(perturbations_path, 'w', libver='latest') as hf:
-    print(len(best_perturbations))
-    for i in range(len(best_perturbations)):
-        data = best_perturbations[i]
-        #print(data)
-        hf.create_dataset(str(idx), data=data, compression='gzip', shape=data.shape, chunks=True)
+with h5py.File(args.data_root + 'correct_predictions.hdf5', 'w') as hf:
+    hf.create_dataset('X', data=correct_predicted_samples_X, shape=correct_predicted_samples_X.shape, compression='gzip', chunks=True)
+    hf.create_dataset('Y', data=correct_predicted_samples_Y, shape=(len(correct_predicted_samples_Y), 1), compression='gzip', chunks=True)
+
+with h5py.File(args.data_root + 'perturbations.hdf5', 'w') as hf:
+    hf.create_dataset('X', data=best_perturbations_X, shape=best_perturbations_X.shape, compression='gzip', chunks=True)
+    hf.create_dataset('Y', data=best_perturbations_Y, shape=(len(best_perturbations_Y), 1), compression='gzip', chunks=True)
 
 
-'''
 
