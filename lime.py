a43d26fb59ab3abc1bb31c2676319daedeb0fc56
Mohammad Hashemi <mohammad99hashemi@gmail.com>
2021-12-28 19:46:51 +0330

    feat: volume mask had to be saved

--- lime.py
+++ lime.py
@@ -12,8 +12,8 @@ from tensorflow.python.client import device_lib
 from sklearn import metrics
 from sklearn.linear_model import LinearRegression
 
-#gpus = tf.config.experimental.list_physical_devices("GPU")
-#tf.config.experimental.set_memory_growth(gpus[0], True)
+gpus = tf.config.experimental.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(gpus[0], True)
 device_name = tf.test.gpu_device_name()
 print("Available Devices: {}".format(device_lib.list_local_devices()))
 print("GPU name: {}".format(tf.test.gpu_device_name()))
@@ -24,10 +24,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data_root', type=str, default='data/', help='path to the root of data directory')
 parser.add_argument('--dataset', type=str, default='dataset_APEX.hdf5', help='dataset name')
 parser.add_argument('--model_path', type=str, default='models/apex_model.h5', help='path to the model')
-parser.add_argument('--weights_path', type=str, default='weights/_fold0_apex_weights.h5', help='path to the model weights for testing')
+parser.add_argument('--weights_path', type=str, default='weights/_fold0_apex_weights.h5',
+                    help='path to the model weights for testing')
 
-parser.add_argument('--iterations', type=int, default=100, help='number of times which we want to apply different random perturbations')
-parser.add_argument('--transformations', type=list, default=['rotate', 'flip_horizontally', 'flip_vertically', 'brightness'], help='just some transformation supported')
+parser.add_argument('--iterations', type=int, default=100,
+                    help='number of times which we want to apply different random perturbations')
+parser.add_argument('--transformations', type=list,
+                    default=['rotate', 'flip_horizontally', 'flip_vertically', 'brightness'],
+                    help='just some transformation supported')
 parser.add_argument('--n_pert', type=int, default=10, help='number of random generated perturbations for each sample')
 parser.add_argument('--steps', type=int, default=10, help='Steps which data generated to be saved')
 args = parser.parse_args()
@@ -35,6 +39,7 @@ args = parser.parse_args()
 # create data loader
 data_loader = DataLoader(hdf5_path=args.data_root + args.dataset)
 dset_x, dset_y = data_loader.read_data()
+
 
 class Lime():
     def __init__(self, volume):
@@ -46,7 +51,8 @@ class Lime():
         '''
         superpixels = []
         first_layer = self.volume[:, :, 1]  # layer is a 2D array now
-        temp_volume = np.repeat(first_layer[None, :], 3, axis=0).transpose(1, 2, 0)  # create a 3-layer tensor by repeating the layer
+        temp_volume = np.repeat(first_layer[None, :], 3, axis=0).transpose(1, 2,
+                                                                           0)  # create a 3-layer tensor by repeating the layer
         first_layer_super_pixel = skimage.segmentation.slic(temp_volume,
                                                             n_segments=n_segments,
                                                             compactness=compactness,
@@ -64,16 +70,17 @@ class Lime():
         layers_perturbation = []
         first_layer_n_unique_values = len(np.unique(superpixels[:, :, 0]))
         first_layer_p = np.random.binomial(1, 0.5, size=(1, first_layer_n_unique_values)).squeeze()
-        for i in range(superpixels.shape[-1]): # over volume layers
+        for i in range(superpixels.shape[-1]):  # over volume layers
             n_unique_values = len(np.unique(superpixels[:, :, i]))
             if n_unique_values == first_layer_n_unique_values:
                 layers_perturbation.append(first_layer_p)
             elif n_unique_values < first_layer_n_unique_values:
-                layers_perturbation.append(first_layer_p[:n_unique_values+1])
+                layers_perturbation.append(first_layer_p[:n_unique_values + 1])
             else:
-                layers_perturbation.append(np.pad(first_layer_p, (0, n_unique_values-first_layer_n_unique_values), 'constant'))
-            #p = np.random.binomial(1, 0.5, size=(1, n_unique_values)).squeeze()
-            #layers_perturbation.append(p)
+                layers_perturbation.append(
+                    np.pad(first_layer_p, (0, n_unique_values - first_layer_n_unique_values), 'constant'))
+            # p = np.random.binomial(1, 0.5, size=(1, n_unique_values)).squeeze()
+            # layers_perturbation.append(p)
 
         return layers_perturbation
 
@@ -98,7 +105,7 @@ class Lime():
         return perturbed_volume, mask_volume
 
     def extract_best_superpixels(self, perts, predictions, num_top_features=4):
-        #num_superpixels = perts.shape[-1]
+        # num_superpixels = perts.shape[-1]
         best_superpixels = []
         layer_1 = []
         layer_2 = []
@@ -128,6 +135,8 @@ class Lime():
 
         best_superpixels = np.array(best_superpixels)
         return best_superpixels
+
+
 # load trained model for evaluating
 print("Model loaded from: {}".format(args.model_path))
 model = tf.keras.models.load_model(args.model_path)
@@ -141,9 +150,9 @@ data = tf.data.Dataset.from_tensor_slices((dset_x, dset_y))
 print("Preparing datasets ...")
 dataset = (
     data
-    .map(augmentation.validation_preprocessing)
-    .batch(1)       # since we want to have prediction for each sample
-    .prefetch(2)
+        .map(augmentation.validation_preprocessing)
+        .batch(1)  # since we want to have prediction for each sample
+        .prefetch(2)
 )
 data_size = len(data)
 # we don't need these anymore
@@ -162,63 +171,64 @@ with tf.device(device_name=device_name):
         print('Processing on sample:{}/{}'.format(idx, data_size))
         volume = sample[0][0, :, :, :, 0]  # volume shape must be (128, 128, 3)
         target = sample[1].numpy()[0, 0]
-        temp_volume = tf.expand_dims(volume, axis=3)
-        temp_volume = tf.expand_dims(temp_volume, axis=0)
-        pred = model.predict(temp_volume)[0,0]
-        if (pred >= 0.5 and target == 0) or (pred < 0.5 and target == 1): # wrong prediction and pass it
-            print("IGNORED. The prediction was wrong.")
-            idx += 1
-            continue
-        else:
-            correct_predicted_samples_X.append(volume)
-            correct_predicted_samples_Y.append(target)
-        lime = Lime(volume)
-        best_pred = 0.5
-        best_volume = None  # generated volume by lime with the most accurate prediction
-        best_idx = 0
-        perts = []
-        predictions = []
-        superpixels = lime.generate_segmentation(max_iter=args.iterations, n_segments=25, compactness=0.3)
-        for i in tqdm(range(args.n_pert)):
-            layers_perturbation = lime.generate_perturbations(superpixels)
-            perturbed_volume, _ = lime.apply_perturbations(layers_perturbation, superpixels)
-            temp_volume = tf.expand_dims(perturbed_volume, axis=3)
+        if target == 1:
+            temp_volume = tf.expand_dims(volume, axis=3)
             temp_volume = tf.expand_dims(temp_volume, axis=0)
-            pred = model.predict(temp_volume)[0,0]
-            perts.append(layers_perturbation)
-            predictions.append(pred)
-            if target == 1:
-                if  pred > best_pred:
-                    best_pred = pred
-                    best_volume = perturbed_volume
-                    best_idx = i
-            else:   # target = 0
-                if  pred < best_pred:
-                    best_pred = pred
-                    best_volume = perturbed_volume
-                    best_idx = i
-        #perts = np.array(perts)
-        #predictions = np.array(predictions)
-        best_superpixels = lime.extract_best_superpixels(perts, predictions, num_top_features=1)
-        mask = []
+            pred = model.predict(temp_volume)[0, 0]
+            if (pred >= 0.5 and target == 0) or (pred < 0.5 and target == 1):  # wrong prediction and pass it
+                print("IGNORED. The prediction was wrong.")
+                idx += 1
+                continue
+            else:
+                correct_predicted_samples_X.append(volume)
+                correct_predicted_samples_Y.append(target)
+            lime = Lime(volume)
+            best_pred = 0.5
+            best_volume = None  # generated volume by lime with the most accurate prediction
+            best_idx = 0
+            perts = []
+            predictions = []
+            superpixels = lime.generate_segmentation(max_iter=args.iterations, n_segments=25, compactness=0.5)
+            for i in tqdm(range(args.n_pert)):
+                layers_perturbation = lime.generate_perturbations(superpixels)
+                perturbed_volume, _ = lime.apply_perturbations(layers_perturbation, superpixels)
+                temp_volume = tf.expand_dims(perturbed_volume, axis=3)
+                temp_volume = tf.expand_dims(temp_volume, axis=0)
+                pred = model.predict(temp_volume)[0, 0]
+                perts.append(layers_perturbation)
+                predictions.append(pred)
+                if target == 1:
+                    if pred > best_pred:
+                        best_pred = pred
+                        best_volume = perturbed_volume
+                        best_idx = i
+                else:  # target = 0
+                    if pred < best_pred:
+                        best_pred = pred
+                        best_volume = perturbed_volume
+                        best_idx = i
+            best_superpixels = lime.extract_best_superpixels(perts, predictions, num_top_features=1)
+            mask = []
 
-        for i in range(3):
-            layer_mask = np.array([0] * len(perts[0][i]))
-            layer_mask[best_superpixels[i]] = True  # Activate top superpixels
-            mask.append(layer_mask)
-        final_perturbed_volume, mask_volume = lime.apply_perturbations(mask, superpixels)
-        #plot_volume(mask_volume, save_fig=False, filename='fig_'+str(idx))
-        temp_volume = tf.expand_dims(final_perturbed_volume, axis=3)
-        temp_volume = tf.expand_dims(temp_volume, axis=0)
-        print("Best superpixel model prediction: {}".format(model.predict(temp_volume)[0,0]))
-        print("The {}/{} perturbation with class {} is chosen with prediction score: {}".format(best_idx, args.n_pert, target, best_pred))
+            for i in range(3):
+                layer_mask = np.array([0] * len(perts[0][i]))
+                layer_mask[best_superpixels[i]] = True  # Activate top superpixels
+                mask.append(layer_mask)
+            final_perturbed_volume, mask_volume = lime.apply_perturbations(mask, superpixels)
+            # plot_volume(final_perturbed_volume, save_fig=True, filename='fig_'+str(idx))
+            temp_volume = tf.expand_dims(final_perturbed_volume, axis=3)
+            temp_volume = tf.expand_dims(temp_volume, axis=0)
+            print("Best superpixel model prediction: {}".format(model.predict(temp_volume)[0, 0]))
+            print(
+                "The {}/{} perturbation with class {} is chosen with prediction score: {}".format(best_idx, args.n_pert,
+                                                                                                  target, best_pred))
 
         # save the results
-        if best_volume is not None and target == 1: # only save the samples with class 1
+        if target == 1 and best_volume is not None:  # only save the samples with class 1
             best_perturbations_X.append(final_perturbed_volume)
             best_perturbations_Y.append(target)
             indices.append(idx)
-            best_mask_volumes.append(best_mask_volumes)
+            best_mask_volumes.append(mask_volume)
         else:
             print()
         idx += 1
@@ -227,46 +237,66 @@ with tf.device(device_name=device_name):
             correct_predicted_samples_Y_array = np.array(correct_predicted_samples_Y)
             best_perturbations_X_array = np.array(best_perturbations_X)
             best_perturbations_Y_array = np.array(best_perturbations_Y)
-            with h5py.File(args.data_root + 'correct_predictions.hdf5', 'w') as hf:
+            best_mask_volumes_array = np.array(best_mask_volumes)
+            with h5py.File(args.data_root + 'correct_predictions_apex_1.hdf5', 'w') as hf:
                 hf.create_dataset('idx', data=np.array(indices), shape=np.array(indices).shape,
                                   compression='gzip', chunks=True)
-                hf.create_dataset('X', data=correct_predicted_samples_X_array, shape=correct_predicted_samples_X_array.shape,
+                hf.create_dataset('X', data=correct_predicted_samples_X_array,
+                                  shape=correct_predicted_samples_X_array.shape,
                                   compression='gzip', chunks=True)
-                hf.create_dataset('Y', data=correct_predicted_samples_Y_array, shape=(len(correct_predicted_samples_Y_array), 1),
+                hf.create_dataset('Y', data=correct_predicted_samples_Y_array,
+                                  shape=(len(correct_predicted_samples_Y_array), 1),
                                   compression='gzip', chunks=True)
-            print('{} correct predictions saved at: {}'.format(correct_predicted_samples_X_array.shape[0], args.data_root + 'correct_predictions.hdf5'))
-            with h5py.File(args.data_root + 'perturbations.hdf5', 'w') as hf:
+            print('{} correct predictions saved at: {}'.format(correct_predicted_samples_X_array.shape[0],
+                                                               args.data_root + 'correct_predictions_apex_1.hdf5'))
+            with h5py.File(args.data_root + 'perturbations_apex_1.hdf5', 'w') as hf:
                 hf.create_dataset('idx', data=np.array(indices), shape=np.array(indices).shape,
                                   compression='gzip', chunks=True)
-                hf.create_dataset('mask', data=np.array(best_mask_volumes), shape=np.array(best_mask_volumes).shape,
-                                  compression='gzip', chunks=True)
-                hf.create_dataset('X', data=best_perturbations_X_array, shape=best_perturbations_X_array.shape, compression='gzip',
+                hf.create_dataset('X', data=best_perturbations_X_array, shape=best_perturbations_X_array.shape,
+                                  compression='gzip',
                                   chunks=True)
                 hf.create_dataset('Y', data=best_perturbations_Y_array, shape=(len(best_perturbations_Y_array), 1),
                                   compression='gzip', chunks=True)
-            print('best perturbations saved at: {}'.format(args.data_root + 'perturbations.hdf5'))
+            print('best perturbations saved at: {}'.format(args.data_root + 'perturbations_apex_1.hdf5'))
+            with h5py.File(args.data_root + 'masks_apex_1.hdf5', 'w') as hf:
+                hf.create_dataset('idx', data=np.array(indices), shape=np.array(indices).shape,
+                                  compression='gzip', chunks=True)
+                hf.create_dataset('mask', data=best_mask_volumes_array, shape=best_mask_volumes_array.shape,
+                                  compression='gzip', chunks=True)
+                hf.create_dataset('Y', data=best_perturbations_Y_array, shape=(len(best_perturbations_Y_array), 1),
+                                  compression='gzip', chunks=True)
+            print('best masks saved at: {}'.format(args.data_root + 'masks_apex_1.hdf5'))
 
             del correct_predicted_samples_X_array
             del correct_predicted_samples_Y_array
             del best_perturbations_X_array
             del best_perturbations_Y_array
-
+            del best_mask_volumes_array
 
 correct_predicted_samples_X = np.array(correct_predicted_samples_X)
 correct_predicted_samples_Y = np.array(correct_predicted_samples_Y)
 best_perturbations_X = np.array(best_perturbations_X)
 best_perturbations_Y = np.array(best_perturbations_Y)
+best_mask_volumes_array = np.array(best_mask_volumes)
 
-with h5py.File(args.data_root + 'correct_predictions.hdf5', 'w') as hf:
+with h5py.File(args.data_root + 'correct_predictions_apex_1.hdf5', 'w') as hf:
     hf.create_dataset('idx', data=np.array(indices), shape=np.array(indices).shape, compression='gzip', chunks=True)
-    hf.create_dataset('X', data=correct_predicted_samples_X, shape=correct_predicted_samples_X.shape, compression='gzip', chunks=True)
-    hf.create_dataset('Y', data=correct_predicted_samples_Y, shape=(len(correct_predicted_samples_Y), 1), compression='gzip', chunks=True)
+    hf.create_dataset('X', data=correct_predicted_samples_X, shape=correct_predicted_samples_X.shape,
+                      compression='gzip', chunks=True)
+    hf.create_dataset('Y', data=correct_predicted_samples_Y, shape=(len(correct_predicted_samples_Y), 1),
+                      compression='gzip', chunks=True)
 
-with h5py.File(args.data_root + 'perturbations.hdf5', 'w') as hf:
+with h5py.File(args.data_root + 'perturbations_apex_1.hdf5', 'w') as hf:
     hf.create_dataset('idx', data=np.array(indices), shape=np.array(indices).shape, compression='gzip', chunks=True)
-    hf.create_dataset('mask', data=np.array(best_mask_volumes), shape=np.array(best_mask_volumes).shape, compression='gzip', chunks=True)
+    hf.create_dataset('mask', data=np.array(best_mask_volumes), shape=np.array(best_mask_volumes).shape,
+                      compression='gzip', chunks=True)
     hf.create_dataset('X', data=best_perturbations_X, shape=best_perturbations_X.shape, compression='gzip', chunks=True)
-    hf.create_dataset('Y', data=best_perturbations_Y, shape=(len(best_perturbations_Y), 1), compression='gzip', chunks=True)
+    hf.create_dataset('Y', data=best_perturbations_Y, shape=(len(best_perturbations_Y), 1), compression='gzip',
+                      chunks=True)
 
-
-
+with h5py.File(args.data_root + 'masks_apex_1.hdf5', 'w') as hf:
+    hf.create_dataset('idx', data=np.array(indices), shape=np.array(indices).shape, compression='gzip', chunks=True)
+    hf.create_dataset('mask', data=best_mask_volumes_array, shape=best_mask_volumes_array.shape, compression='gzip',
+                      chunks=True)
+    hf.create_dataset('Y', data=best_perturbations_Y, shape=(len(best_perturbations_Y), 1), compression='gzip',
+                      chunks=True)
