b3c45dc763f47ae98cb3bfdca53b811606b31493
Mohammad Hashemi <mohammad99hashemi@gmail.com>
2022-02-14 22:13:28 +0330

    fix: small changes applied

--- segmentation/train_unet.py
+++ segmentation/train_unet.py
@@ -1,10 +1,12 @@
 import argparse
 import os
-from sklearn.model_selection import train_test_split
+import json
 import torch
+from tqdm import tqdm
 import torch.nn as nn
 from torch.utils.data import DataLoader, random_split
-import torchgeometry as tgm
+import warnings
+warnings.filterwarnings('ignore')
 
 from data_loader import LoadDataset
 from attention_unet import UNet3D
@@ -14,18 +16,31 @@ parser = argparse.ArgumentParser()
 # directories
 parser.add_argument('--data_root', type=str, default='data/', help='path to the root of data directory')
 parser.add_argument('--images_path', type=str, default='apex_lime/correct_predictions_apex.hdf5', help='images path')
-parser.add_argument('--target_path', type=str, default='apex_lime/perturbations_apex.hdf5', help='targets path')
+parser.add_argument('--target_path', type=str, default='apex_lime/masks_apex.hdf5', help='targets path')
+parser.add_argument('--exp_dir', type=str, default='segmentation/experiments/', help='saved experiences directory')
+parser.add_argument('--exp_no', type=str, default='00', help='experiment number')
 
 # training
 parser.add_argument('--epochs', type=int, default=50, help='number of epochs')
-parser.add_argument('--log_step', type=int, default=5, help='steps to print the loss and ...')
 parser.add_argument('--test_size', type=int, default=0.2, help='percentage of test size')
 parser.add_argument('--batch_size', type=int, default=8, help='batch_size')
-parser.add_argument('--num_workers', type=int, default=1, help='number of workers')
+parser.add_argument('--num_workers', type=int, default=2, help='number of workers')
 parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=1e-8, help='weight decay rate')
 args = parser.parse_args()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+if not os.path.exists(args.exp_dir + args.exp_no):
+    os.makedirs(args.exp_dir + args.exp_no)
+# Initialize logging
+training_configs = dict(exp=args.exp_no, epochs=args.epochs, batch_size=args.batch_size, learning_rate=args.learning_rate,
+                              test_size=args.test_size, device=device.type)
+json_path = args.exp_dir + args.exp_no + f"/{args.exp_no}.json"
+with open(json_path, "w") as jf:
+    json.dump(training_configs, jf)
+print(training_configs)
 
 # load the images and targets
 loader_args = dict(batch_size=args.batch_size,
@@ -39,49 +54,64 @@ dataset = LoadDataset(images_hdf5_path=args.data_root + args.images_path,
 n_test = int(len(dataset) * args.test_size)
 n_train = len(dataset) - n_test
 train_set, test_set = random_split(dataset, [n_train, n_test], generator=torch.Generator().manual_seed(0))
-train_loader = DataLoader(train_set, shuffle=False)
-test_loader = DataLoader(test_set, shuffle=False)
-
+train_loader = DataLoader(train_set, shuffle=False, **loader_args)
+test_loader = DataLoader(test_set, shuffle=False, **loader_args)
 
 # model creation
 unet = UNet3D(in_channels=1,
               out_channels=1,
-              final_sigmoid=False)
+              final_sigmoid=True)
+unet.to(device=device)
 
 # set up optimizer, the loss, learning rate and etc.
 optimizer = torch.optim.RMSprop(unet.parameters(),
                                 lr=args.learning_rate,
                                 weight_decay=args.weight_decay,
                                 momentum=args.momentum)
-criterion = nn.CrossEntropyLoss()
-
+criterion = nn.BCEWithLogitsLoss()
+epoch_train_loss_list = []
+epoch_test_loss_list = []
 # start training
 for epoch in range(args.epochs):
     unet.train()
-    for batch in train_loader:
-        images = batch['image']#.permute(0, 1, 4, 3, 2)
-        targets = batch['target']#.permute(0, 1, 4, 3, 2)
-
-        prediction, pool_fea = unet(images)
-        train_loss = criterion(prediction, targets) + dice_loss(prediction[:, 0, :, :, :], targets[:, 0, :, :, :])
-
-        optimizer.zero_grad()
-        train_loss.backward()
-        optimizer.step()
-
-    # evaluate
-    if epoch % args.log_step:
-        print("Epoch[{0}/{1}], Train loss:{2}".format(epoch + 1, args.epochs, train_loss))
-        for batch in test_loader:
-            images = batch['image']  # .permute(0, 1, 4, 3, 2)
-            targets = batch['target']  # .permute(0, 1, 4, 3, 2)
-            with torch.no_grad():
-                # predict the mask
+    try:
+        with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{args.epochs}', unit='img') as pbar:
+            for batch in train_loader:
+                images = batch['image'].to(device=device, dtype=torch.float32)
+                targets = batch['target'].to(device=device, dtype=torch.float32)
                 prediction, pool_fea = unet(images)
+                train_loss = dice_loss(prediction[:, 0, :, :, :], targets[:, 0, :, :, :])
+                epoch_train_loss_list.append(train_loss)
 
-                test_loss = criterion(prediction, targets) + dice_loss(prediction[:, 0, :, :, :],
-                                                                        targets[:, 0, :, :, :])
-                print("Epoch[{0}/{1}], Test loss:{2}".format(epoch + 1, args.epochs, test_loss))
+                optimizer.zero_grad()
+                train_loss.backward()
+                optimizer.step()
+                pbar.update(images.shape[0])
+
+            avg_train_loss = torch.mean(torch.tensor(epoch_train_loss_list))
+
+            # evaluate
+            for batch in test_loader:
+                images = batch['image'].to(device=device, dtype=torch.float32)
+                targets = batch['target'].to(device=device, dtype=torch.float32)
+                with torch.no_grad():
+                    # predict the mask
+                    prediction, pool_fea = unet(images)
+                    test_loss = dice_loss(prediction[:, 0, :, :, :], targets[:, 0, :, :, :])
+                    epoch_test_loss_list.append(test_loss)
+            avg_test_loss = torch.mean(torch.tensor(epoch_test_loss_list))
+
+            log_file_path = args.exp_dir + args.exp_no + f"dice_score.txt"
+            log = f"Train loss: {avg_train_loss}, Test loss:{avg_test_loss}\n"
+            print(log)
+            with open(log_file_path, "a") as f:
+                f.write(log)
+            f.close()
+        print(f"Model saved at {args.exp_dir}{args.exp_no}/exp{args.exp_no}.pth")
+        torch.save(unet.state_dict(), f"{args.exp_dir}{args.exp_no}/exp{args.exp_no}.pth")
+    except KeyboardInterrupt:
+        print(f"Model saved at {args.exp_dir}{args.exp_no}/exp{args.exp_no}.pth")
+        torch.save(unet.state_dict(), f"{args.exp_dir}{args.exp_no}/exp{args.exp_no}.pth")
 
 
 
